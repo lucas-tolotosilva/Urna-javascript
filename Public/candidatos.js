6e7fd2c3cdec14ed4384de6a826303451c61cf53
lucastoloto <lucassilva.toloto@hotmail.com>
2021-11-08 18:29:01 -0300

    Adição dos Candidatos,Vices e suas respectivas fotos. Ao digitar um número referente a um determinado candidato, aparece todos os dados na tela

---
+++ Public/candidatos.js
@@ -0,0 +1,23 @@
+const candidato1 = {
+    nome_candidato:"Neymar Júnior",
+    foto_candidato:"../img/candidatos/neymar.jpg",
+    partido:"Paris Saint Germain",
+    nome_vice:"Bruna Marquezine",
+    foto_vice:"../img/candidatos/bruna.jpg"
+};
+
+const candidato2 = {
+    nome_candidato:"Craque Neto",
+    foto_candidato:"../img/candidatos/neto.jpg",
+    partido:"Curintia",
+    nome_vice:"Felipe Melo",
+    foto_vice:"../img/candidatos/felipemelo.jpg"
+};
+
+const candidato3 = {
+    nome_candidato:"Marcelinho Carioca",
+    foto_candidato:"../img/candidatos/marcelinho.jpg",
+    partido:"Pé de Anjo",
+    nome_vice:"Vanderlei Luxemburgo",
+    foto_vice:"../img/candidatos/luxa.jpg"
+};
--- Public/script.js
+++ Public/script.js
@@ -1,5 +1,105 @@
+console.log(candidato1);
+console.log(candidato2);
+console.log(candidato3);
 
+let input = document.getElementById("input");
+//const container = document.getElementById("container");
 
+// função que aciona os botões
 function acionabtn(value_btn){
-    console.log(value_btn)
+    for(i=0; i<10; i++){
+            passarValorTela(value_btn,i);
+    }
+    testaTamanhoInput();
+
+    
 }
+
+// Função responsável por passar o valor do botão para o input 
+function passarValorTela(value_input,i){
+    if(value_input == i){
+        input.value = input.value + i;
+    }
+
+}
+
+// Funcão que testa quantos números(string) tem no input
+function testaTamanhoInput(){
+    input.value.toString()
+
+    if(input.value.length == 2){
+        console.log("passando 2 números")
+    }
+    
+    imprimeCandidatos();
+}
+
+// função que passará os dados de cada candidato
+function imprimeCandidatos(){
+    if(input.value == 10){
+        setCandidato1()
+    } else if(input.value == 20){
+        setCandidato2()
+    } else if(input.value == 30){
+        setCandidato3()
+    }
+}
+
+// Pegando e passando os dados do objeto candidato 1 
+function setCandidato1(){
+    const nome = candidato1.nome_candidato;
+        document.getElementById("nome").innerHTML = nome;
+        
+        const partido = candidato1.partido;
+        document.getElementById("partido").innerHTML = partido;
+
+        const nome_vice = candidato1.nome_vice;
+        document.getElementById("nome-vice").innerHTML = nome_vice;
+
+        const img_candidato = document.getElementById("presidente");
+        const set_img_candidato = presidente.setAttribute("src",candidato1.foto_candidato);
+
+        const img_vice = document.getElementById("vice");
+        const set_img_vice = vice.setAttribute("src",candidato1.foto_vice);
+}
+
+// Pegando e passando os dados do objeto candidato 2 
+function setCandidato2(){
+    const nome = candidato2.nome_candidato;
+        document.getElementById("nome").innerHTML = nome;
+        
+        const partido = candidato2.partido;
+        document.getElementById("partido").innerHTML = partido;
+
+        const nome_vice = candidato2.nome_vice;
+        document.getElementById("nome-vice").innerHTML = nome_vice;
+
+        const img_candidato = document.getElementById("presidente");
+        const set_img_candidato = presidente.setAttribute("src",candidato2.foto_candidato);
+
+        const img_vice = document.getElementById("vice");
+        const set_img_vice = vice.setAttribute("src",candidato2.foto_vice);
+}
+
+// Pegando e passando os dados do objeto candidato 3
+function setCandidato3(){
+    const nome = candidato3.nome_candidato;
+        document.getElementById("nome").innerHTML = nome;
+        
+        const partido = candidato3.partido;
+        document.getElementById("partido").innerHTML = partido;
+
+        const nome_vice = candidato3.nome_vice;
+        document.getElementById("nome-vice").innerHTML = nome_vice;
+
+        const img_candidato = document.getElementById("presidente");
+        const set_img_candidato = presidente.setAttribute("src",candidato3.foto_candidato);
+
+        const img_vice = document.getElementById("vice");
+        const set_img_vice = vice.setAttribute("src",candidato3.foto_vice);
+}
+
+/*function aciona_btncorrige(){
+   imprimeCandidatos() = "";
+   input.focus();
+} */
